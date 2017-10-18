63cfe925e79d2a10f68ca5b2c540da56fcab0fe5
alex and omega <tehpeterson@gmail.com>
2017-10-18 08:45:27 -0700

    updated repo

--- votomatic/votomatic.py
+++ votomatic/votomatic.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-import discord, asyncio, random, re, settings, time 
+import discord, asyncio, random, re, settings, time, textwrap
 from votomaticDB import props
 from votomaticDB import votes
 from votomaticDB import comments
 from votomaticDB import timers
 from discord.ext.commands import Bot
 salt='afgagsfghsdfhssdag'
+
+global BotUser, organicTimer
 global settings, pendingBills, mIds, messageDeque
 settings=settings.Settings()
 messageDeque=list()
 pendingBills=list()
 mIds=list()
+organicTimer=dict()
 def log(author, channel):
   return 
 votomatic=Bot(command_prefix='vote!')
@@ -29,6 +32,7 @@ async def voteEmbed(message, data, votes=list(), arguments=list()):
   aCard.set_author(name=message.author.name, icon_url=message.author.avatar_url)
   aCard.set_footer(text='%s      vote code:%s'  % ( time.ctime(data.endDate), str(data.id)), icon_url=message.author.avatar_url)
   aCard.title=data.title
+ # aCard.description=data.body
   aCard.url='http://google.com'  
   aCard.type='rich'
   aCard.set_image(url=images[data.outcome])
@@ -44,16 +48,19 @@ async def voteEmbed(message, data, votes=list(), arguments=list()):
   #aCard.add_field(name='Vote Ends:', value=time.ctime(data.endDate))
   return aCard
 async def editor(edit, id):
+    global mIds
     newMsg=await edit
-    print(newMsg.id)
     prop=props.updateProp(id, newMsg.id)
-    print(prop.messageID)
-    mIds.append(newMsg.id)
+    if newMsg.id not in mIds:
+        mIds.append(newMsg.id)
     return newMsg
 def schedule_coroutine(target, *, loop=None):
     return asyncio.ensure_future(target, loop)
 async def billWatch(bill):
-    while time.time()<bill.endDate:
+    while True:
+        if bill.complete or int(time.time())>bill.endDate:
+            break
+        print('pulse for ' + str(bill.id))
         await asyncio.sleep(10)
     bill=await closeProp(bill.id)
 async def closeProp(id):
@@ -63,11 +70,85 @@ async def closeProp(id):
         prop=props.closeProp(id)
         channel=votomatic.get_channel(id=prop.channelID)
         message=await votomatic.get_message(channel, prop.messageID)
-        count=votes.getVotes(id)
-        results=await redrawEmbed(message)
-        this=await votomatic.send_message(channel,id, embed=results)
+        counts=votes.getVotes(id)
+        if prop.title == '0' and prop.body == '0':
+            user=await votomatic.get_user_info(prop.voteOwner)
+            result="The vote is in!\n %s's vote on '*%s*' is complete and result is %i for Yes and %i for No." % (user.mention, message.content, counts[0], counts[1])
+            await votomatic.clear_reactions(message)
+            await destroyAnnounce(message, result, 10)
+        else:
+            results=await redrawEmbed(message)
+            await votomatic.send_message(channel,id, embed=results)
+            await votomatic.delete_message(message)
         mIds.remove(message.id)
     return True
+async def redrawEmbed(message):
+    counts=votes.getVotes(int(message.content))
+    cmmnts=comments.getComments(int(message.content))
+    pData=props.get(int(message.content))
+    refresh=await voteEmbed(message, pData, counts, arguments=cmmnts)
+    return refresh
+@votomatic.event
+async def on_ready():
+    global BotUser
+    BotUser=await votomatic.get_user_info('313178786692595713')
+    await pendingLoop()
+ #   asyncio.ensure_future(baName(), loop=None)
+    print("Client logged in")
+    return
+async def pendingLoop():
+    global pendingBills, mIds, messageDeque
+    bills=props.getPending()
+    pendingBills=list()
+    if bills != None: 
+        for bill in bills:
+            message=await votomatic.get_message(votomatic.get_channel(bill.channelID), id=bill.messageID)
+            votomatic.messages.append(message)
+            print(bill)
+            await getOfflineVotes(bill)
+            if bill.endDate<int(time.time()):
+                mIds.append(bill.messageID) 
+                await closeProp(bill.id)
+            else:    
+                mIds.append(bill.messageID) 
+                pendingBills.append(asyncio.ensure_future(billWatch(bill), loop=None))
+    return
+async def getOfflineVotes(prop):
+    channel=votomatic.get_channel(prop.channelID)
+    message=await votomatic.get_message(channel, id=prop.messageID)
+    voteChanged=False
+    vts={
+        '✅':True, 
+        '❌':False 
+        }
+    for vote in ['✅', '❌']:
+        print('getOfflineVotes, %s' % vote)
+        vote=discord.Reaction(message=message, emoji=vote)
+        members=await votomatic.get_reaction_users(vote, limit=100)
+        print(members)
+        for member in members:
+            if member.id != BotUser.id:
+                print('getOfflineVotes, member.id != BotUser.id %s' % vote.emoji)
+                voteChanged=True
+                voted=votes.voteOn(member.id, prop.id, vts[vote.emoji])
+                await votomatic.remove_reaction(message=message, emoji=vote.emoji, member=member)
+    for vote in ['⭕']:    
+        print('getOfflineVotes, %s' % vote)
+        vote=discord.Reaction(message=message, emoji=vote)
+        members=await votomatic.get_reaction_users(vote, limit=100)
+        for member in members:
+            if member.id != BotUser.id:
+                print('getOfflineVotes, member.id != BotUser.id %s' % vote.emoji)
+                voteChanged=True
+                curVote=votes.getVote(member.id, prop.id)
+                if curVote:
+                    votes.deleteVote(curVote.id)                
+                await votomatic.remove_reaction(message, emoji=vote.emoji, member=member)
+    if voteChanged: 
+        print('getOfflineVotes, voteChanged')
+        Embed=await redrawEmbed(message)
+        await votomatic.edit_message(message, prop.id, embed=Embed)
+    return
 @votomatic.event
 async def on_reaction_add(reaction, user):
     print('add_reaction block')
@@ -77,54 +158,69 @@ async def on_reaction_add(reaction, user):
     vts={'✅':True,
            '❌':False,
            '⭕':'' }
+    announce=''
     if reaction.message.id in mIds and reaction.emoji in ['✅', '❌', '⭕']:
+        prop=props.propByMsgID(reaction.message.id)
+        vote=votes.getVote(user.id, int(prop.id))
         if reaction.emoji in ['⭕']:
-            vote=votes.getVote(user.id, int(reaction.message.content))
             if vote:
+                announce='%s uses %s to clear their vote.' % (user.mention, reaction.emoji)
                 votes.deleteVote(vote.id)
+            else:
+                await votomatic.remove_reaction(reaction.message, reaction.emoji, user)
+                return
         else:
-            vote=votes.voteOn(user.id, int(reaction.message.content), vts[reaction.emoji])
+            if vote and vote.vote==vts[reaction.emoji]:
+                await votomatic.remove_reaction(reaction.message, reaction.emoji, user)
+                return
+            announce='%s votes %s.' % (user.mention, reaction.emoji)
+            vote=votes.voteOn(user.id, prop.id, vts[reaction.emoji])
         await votomatic.remove_reaction(reaction.message, reaction.emoji, user)
-        msgEmbed=await redrawEmbed(reaction.message)
-        counts=votes.getVotes(int(reaction.message.content))
-        msgEmbed.set_footer
-        edit=votomatic.edit_message(reaction.message, reaction.message.content, embed=msgEmbed)
-        edited=await editor(edit, reaction.message.content)
-        mIds.remove(reaction.message.id)
+        if prop and prop.title != '0':
+            msgEmbed=await redrawEmbed(reaction.message)
+            edit=votomatic.edit_message(reaction.message, reaction.message.content, embed=msgEmbed)
+            edited=await editor(edit, reaction.message.content)
+        else:
+            asyncio.ensure_future(destroyAnnounce(reaction.message, announce, time=4), loop=None)
+    elif reaction.message.id in mIds and reaction.emoji not in ['✅', '❌', '⭕']:
+        await votomatic.remove_reaction(reaction.message, reaction.emoji, user)
     return
-async def redrawEmbed(message):
-    counts=votes.getVotes(int(message.content))
-    cmmnts=comments.getComments(int(message.content))
-    pData=props.get(int(message.content))
-    refresh=await voteEmbed(message, pData, counts, arguments=cmmnts)
-    return refresh
-@votomatic.event
-async def on_ready():
-    await pendingLoop()
-    print("Client logged in")
+async def destroyAnnounce(message, content, time=2):
+    annce=await votomatic.send_message(message.channel, content=content)
+    await asyncio.sleep(time)
+    await votomatic.delete_message(annce)
     return
-async def pendingLoop():
-    global pendingBills, mIds, messageDeque
-    bills=props.getPending()
-    pendingBills=list()
-    if bills != None: 
-        for bill in bills:
-            print(bill)
-            mIds.append(bill.messageID) 
-            pendingBills.append(await billWatch(bill))
-    return
-@votomatic.event
-async def on_message(message):
-    print(message.content)
-    if message.author.bot or message.channel.is_private:
-        return
-    await votomatic.process_commands(message)
-    return 
 @votomatic.command(pass_context=True)
-async def c(msg):
+async def r(msg, amount):
     global mIds
     msg=msg.message
-    parser='\s(\d+)\|(.+)'
+    amount=int(amount)
+    bill=props.getProp(amount)
+    if bill:
+        if bill.title == '0' and bill.body == '0':
+            return 
+        if bill.complete != True:
+            channel=votomatic.get_channel(bill.channelID)
+            message=await votomatic.get_message(channel, id=bill.messageID)
+            Embed=await redrawEmbed(message)
+            newMessage=await votomatic.send_message(channel, message.content, embed=Embed)
+            for emoji in ['✅', '❌', '⭕']:
+                await votomatic.add_reaction(newMessage, emoji)
+            props.updateProp(amount, newMessage.id)
+            mIds.remove(message.id)
+            await votomatic.delete_message(message)
+            mIds.append(newMessage.id)
+            if channel != msg.channel: 
+                await votomatic.say('vote %i bumped to front of %s' % (bill.id, channel.mention))
+        elif bill.complete==True:
+            await votomatic.say('Vote already complete!')
+    else:
+        await votomatic.say('Vote not found/invalid vote code.')
+    return True         
+@votomatic.command(pass_context=True)
+async def c(msg):
+    msg=msg.message
+    parser='\s(\d+)\|([\w|\n|\s|\W]*)'
     parsed=re.search(parser, msg.content)
     if parsed != None: 
         id=parsed.group(1)
@@ -133,26 +229,35 @@ async def c(msg):
         votomatic.say('*comments should be in the following format:*  vote!comment vote code: this is the body')
         return False
     prop=props.get(int(id))
+    if prop.title==0 and prop.body==0:
+        return
     message=await votomatic.get_message(msg.channel, id=prop.messageID)
+    wrap=textwrap.wrap(cmnt, 980)
+    counter=1
+    length=len(wrap)
+    cmnts=list()
+    for strng in wrap:
+        cmnts.append(strng+'\n -=%i/ %i=-' % (counter, length))
+        counter=counter+1
     if message:
-        comment=comments.addComment(msg.author.id, msg.author.mention, prop.id, cmnt)
-        print(message.content)
+        for comment in cmnts:
+            comments.addComment(msg.author.id, msg.author.mention, prop.id, comment)
         embd=await redrawEmbed(message)
         edit=votomatic.edit_message(message, prop.id, embed=embd)
+        await votomatic.delete_message(msg)
         edited=await editor(edit, int(id))
-        mIds.remove(prop.messageID)
     else:
-        print('relevent message object is missing')
+        return
 @votomatic.command(pass_context=True)
 async def e(msg, amount):
     global mIds
     msg=msg.message
     amount=int(amount)
     bill=props.getProp(amount)
+    if bill.title==0 and bill.body==0:
+        return
     if bill:
         if bill.complete != True and bill.voteOwner==msg.author.id:
-            print(mIds)
-            print(bill.messageID)
             bill=await closeProp(bill.id)
         elif bill != True and bill.voteOwner != msg.author.id: 
             user=await votomatic.get_user_info(bill.voteOwner)
@@ -184,7 +289,7 @@ async def t(msg, amount):
 async def s(msg,arg):
     global pendingBills, mIds
     msg=msg.message
-    parser='\s(.+)\|(.+)'
+    parser='\s(.+)\|([\w|\n|\s|\W]*)'
     parsed=re.search(parser, msg.content)
     if parsed != None: 
         subject=parsed.group(1)
@@ -198,8 +303,77 @@ async def s(msg,arg):
     for emoji in ['✅', '❌', '⭕']:
         await votomatic.add_reaction(propMessage, emoji)
     vData=props.updateProp(vData.id, propMessage.id)
-    print(propMessage.id)
     mIds.append(vData.messageID)
-    pendingBills.append(await billWatch(vData))
+    pendingBills.append(asyncio.ensure_future(billWatch(vData), loop=None))
     return True      
+@votomatic.event
+async def on_message(message):
+    if message.author.bot or message.channel.is_private:
+        return
+    await votomatic.process_commands(message)
+    global pendingBills, mIds, organicTimer
+    msg=message
+    channel=msg.channel
+    parser='.*(can vote|put it to a vote|vote on it|[sS]hould [Ii]|[aA]nyone wanna|[aA]nyone else|[dD]o you think|[hH]ave you ever|[sS]hould we).*'
+    parsed=re.search(parser, msg.content)
+    if parsed != None: 
+        if channel.id not in organicTimer.keys() or organicTimer[channel.id]<int(time.time()):
+            vData=props.quickProp(msg.id, msg.channel.id, msg.author.id)
+            propMessage=msg
+            for emoji in ['✅', '❌', '⭕']:
+                await votomatic.add_reaction(propMessage, emoji)
+            mIds.append(vData.messageID)
+            pendingBills.append(asyncio.ensure_future(billWatch(vData), loop=None))
+            organicTimer[channel.id]=int(time.time())+180
+            return
+        else:
+            return
+    else:
+        return
+@votomatic.command(pass_context=True)
+async def board(msg):
+    return
+@votomatic.command(pass_context=True)
+async def mids():
+    print(mIds)
+
+
+
+
+
+
+
+
+
+
+
+#async def baName():
+#     members=list()
+#     for server in votomatic.servers:
+#         me=server.get_member('223564803224895488')
+#         members.append(me)
+#     letterName=[('B','2'), 
+#                 ('a','1'),
+#                 ('n','14')]
+#     name='B.a.n.a.n.a'
+#     while True: 
+#         copy=list(letterName)
+#         perm1=random.choice(copy)
+#         copy.remove(perm1)
+#         perm2=random.choice(copy)
+#         name2=name
+#         for perm in (perm1, perm2):
+#             name2=name2.replace(perm[0], perm[1])
+#         for member in members:
+#             try:
+#                 await votomatic.change_nickname(member, name2)
+#             except:
+#                 print('that is ok')
+#             
+#         sleep=random.randint(30, 60)
+#         await asyncio.sleep(sleep)
+        
+        
+        
+    
 votomatic.run(settings['token'])
